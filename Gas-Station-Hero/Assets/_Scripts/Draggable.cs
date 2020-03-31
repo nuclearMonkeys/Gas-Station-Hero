66f8064cb2d9f7e17f75e74058d68ea15cffc6da
nuclearMonkeys <lyndond@uci.edu>
2020-03-30 17:58:52 -0700

    Preliminary Drag n Drop

---
+++ Gas-Station-Hero/Assets/_Scripts/Draggable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+    private float updatae;
+    private bool isOverCounter;
+
+    private void Update() 
+    {
+        if(isOverCounter) 
+        {
+            transform.position -= new Vector3(0, 9.8f * Time.deltaTime, 0);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) 
+    {
+        Debug.Log("OnBeginDrag");
+        parentToReturnTo = transform.parent;
+        transform.SetParent( transform.parent.parent );
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData) 
+    {
+        Debug.Log("OnDrag");
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) 
+    {
+        Debug.Log("OnEndDrag");
+        transform.SetParent( parentToReturnTo );
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        if(parentToReturnTo.CompareTag("OverTheCounter")) 
+        {
+            isOverCounter = true;
+        }
+    }
+
+    private IEnumerator DropObject() 
+    {
+        
+        yield return new WaitForRealTimeSecs(1.0f);
+    }
+}
---
+++ Gas-Station-Hero/Assets/_Scripts/DropZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData) 
+    {
+        Debug.Log("OnPointerEtner");
+    }
+
+    public void OnPointerExit(PointerEventData eventData) 
+    {
+        Debug.Log("OnPointerExit");
+    }
+
+    public void OnDrop(PointerEventData eventData) 
+    {
+        Debug.Log(eventData.pointerDrag.name + "was dropped on " + gameObject.name);
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if(d) 
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+}
