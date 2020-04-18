4ed4dfc62919afa4c68f2ecf086392bc4bcee9e9
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-04-18 15:56:02 -0700

    framing the line system

---
+++ Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/InLineCustomerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InLineCustomerBehavior : MonoBehaviour
+{
+    //used to make the customer leave the line out into the screen
+    public void leave()
+    {
+
+    }
+    public void moveUp()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
---
+++ Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/LineSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LineSystem : MonoBehaviour
+{
+    //ADJUSTABLE ATTRIBUTES
+    public float customerSpeed;
+
+
+
+
+
+    private const int maxNumCustomer = 6;
+
+    private bool[] SpotEmpty = new bool[maxNumCustomer];
+    private Vector2[] CustomerVectors = new Vector2[maxNumCustomer];
+
+    public GameObject customer_prefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < maxNumCustomer; i++)
+        {
+            SpotEmpty[i] = true;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+}
