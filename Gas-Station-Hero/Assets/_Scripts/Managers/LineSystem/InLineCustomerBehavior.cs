3a2e6bd79fc2ee1a02b46b1629bbe63c2a0c9768
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-04-20 14:45:15 -0700

    line system basic setup

--- Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/InLineCustomerBehavior.cs
+++ Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/InLineCustomerBehavior.cs
@@ -2,27 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InLineCustomerBehavior : MonoBehaviour
+public class InlineCustomerBehavior : MonoBehaviour
 {
-    //used to make the customer leave the line out into the screen
+    public Vector2 TargetLoc;
+    private Vector2 vector;
+    public float speed = 3;
+    private float turnSpeed = 100;
+    private bool leaving = false;
+    private bool moving = false;
+
+
     public void leave()
     {
-
+        vector.x = -speed;
+        vector.y = 0;
+        leaving = true;
+        Destroy(gameObject, 5);
     }
-    public void moveUp()
+    public void init(Vector2 target)
     {
-
+        TargetLoc = target;
+        moving = true;
+        Debug.Log(TargetLoc);
     }
-    // Start is called before the first frame update
-    void Start()
+    public void moveUP()
     {
-        
+        moving = true;
     }
+    
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)vector * Time.deltaTime;
+        if (leaving)
+        {
+            if (vector.x < 0)
+            {
+                vector.x += speed / turnSpeed;
+                vector.y -= speed / turnSpeed;
+            }
+        }
+        else if (moving)
+        {
+            vector = Vector3.MoveTowards(transform.position, TargetLoc, speed) - transform.position;
+            if (vector.magnitude < 0.1) 
+            {
+                moving = false;
+                vector = Vector2.zero;
+            }
+        }
     }
-    
 }
--- Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/LineSystem.cs
+++ Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/LineSystem.cs
@@ -7,31 +7,78 @@ public class LineSystem : MonoBehaviour
     public float customerSpeed;
 
 
+    //TESTING VARIABLES
+    public int counter = 500;
+    
 
 
 
     private const int maxNumCustomer = 6;
-
+    private int totalCustomer;
     private bool[] SpotEmpty = new bool[maxNumCustomer];
-    private Vector2[] CustomerVectors = new Vector2[maxNumCustomer];
+    private GameObject[] customers = new GameObject[maxNumCustomer];
+    public Vector2[] CustomerVectors = new Vector2[maxNumCustomer];
 
     public GameObject customer_prefab;
 
 
-    // Start is called before the first frame update
-    void Start()
+
+
+    //spawn numberof customers outside the door, one at a time
+    public void startDay()
     {
-        for (int i = 0; i < maxNumCustomer; i++)
+
+        StartCoroutine(SpwanCustomer());
+        
+            
+    }
+    IEnumerator SpwanCustomer()
+    {
+        for (int index = 0; index < maxNumCustomer; index++)
         {
-            SpotEmpty[i] = true;
+            yield return new WaitForSeconds(3);
+            customers[index] = Instantiate(customer_prefab, new Vector3(-3, -10, 0), Quaternion.identity);
+            customers[index].GetComponent<InlineCustomerBehavior>().init( CustomerVectors[index]);
         }
     }
 
+    public void MoveLine()
+    {
+        customers[totalCustomer % maxNumCustomer].GetComponent<InlineCustomerBehavior>().leave();
+        StartCoroutine(delayedMove());
+        customers[totalCustomer % maxNumCustomer] = Instantiate(customer_prefab, new Vector3(-2, -10, 0), Quaternion.identity);
+        customers[totalCustomer % maxNumCustomer].GetComponent<InlineCustomerBehavior>().init(CustomerVectors[maxNumCustomer - 1]);
+        totalCustomer++;
+    }
+    IEnumerator delayedMove()
+    {
+        for (int i = 1; i < maxNumCustomer - 1; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            customers[(totalCustomer + i) % maxNumCustomer].GetComponent<InlineCustomerBehavior>().TargetLoc = CustomerVectors[i - 0];
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startDay();
+    }
+
+
+    //returns the total number of customers served;
+    public int getTotalCustomers()
+    {
+        return totalCustomer;
+    }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            MoveLine();
+        }
     }
 
 
