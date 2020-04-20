91d02b1209913b08a03c951568f64e4817fefd9d
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-04-20 15:56:25 -0700

    saved scene!

--- Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/LineSystem.cs
+++ Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/LineSystem.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 public class LineSystem : MonoBehaviour
 {
-    //ADJUSTABLE ATTRIBUTES
-    public float customerSpeed;
-
     private const int maxNumCustomer = 6;
     private int totalCustomer;
     private GameObject[] customers = new GameObject[maxNumCustomer];
