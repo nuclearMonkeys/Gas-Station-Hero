a639957aa2db4dd4e25df3ea9266bda4df4fa6b8
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-04-20 15:49:24 -0700

    simple line system implemented

--- Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/InLineCustomerBehavior.cs
+++ Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/InLineCustomerBehavior.cs
@@ -11,7 +11,10 @@ public class InlineCustomerBehavior : MonoBehaviour
     private bool leaving = false;
     private bool moving = false;
 
-
+    /*=================================
+    fucntion to have the customer leave the line and self destroy after 5 secs,
+    which should be adjusted depending on the speed of the customer and the size of the screen
+    ==================================*/
     public void leave()
     {
         vector.x = -speed;
@@ -19,34 +22,38 @@ public class InlineCustomerBehavior : MonoBehaviour
         leaving = true;
         Destroy(gameObject, 5);
     }
-    public void init(Vector2 target)
+
+    /*=================================
+     * sets the target location to the parameter, and enables movement.
+    ==================================*/
+    public void moveUp(Vector2 target)
     {
         TargetLoc = target;
-        moving = true;
-        Debug.Log(TargetLoc);
-    }
-    public void moveUP()
-    {
         moving = true;
     }
     
 
-
-    // Update is called once per frame
+    /*=================================
+     Update is called once per frame
+    ==================================*/
     void Update()
     {
+        
         transform.position += (Vector3)vector * Time.deltaTime;
-        if (leaving)
-        {
+
+
+
+        if (leaving)        //if the customer is leaving, it will gradually turn depending on the turn speed, 
+        {                   //until x component of the vector becomes 0, leave head straight down.
             if (vector.x < 0)
             {
                 vector.x += speed / turnSpeed;
                 vector.y -= speed / turnSpeed;
             }
         }
-        else if (moving)
+        else if (moving)    //if the customer is not leaving, but moving is enabled, customer will move toward the targetLocation
         {
-            vector = Vector3.MoveTowards(transform.position, TargetLoc, speed) - transform.position;
+            vector = Vector3.MoveTowards(transform.position, TargetLoc, speed) - transform.position;//part to stop moving the customer once it reaches the proximity of the target location.
             if (vector.magnitude < 0.1) 
             {
                 moving = false;
--- Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/LineSystem.cs
+++ Gas-Station-Hero/Assets/_Scripts/Managers/LineSystem/LineSystem.cs
@@ -6,57 +6,65 @@ public class LineSystem : MonoBehaviour
     //ADJUSTABLE ATTRIBUTES
     public float customerSpeed;
 
-
-    //TESTING VARIABLES
-    public int counter = 500;
-    
-
-
-
     private const int maxNumCustomer = 6;
     private int totalCustomer;
-    private bool[] SpotEmpty = new bool[maxNumCustomer];
     private GameObject[] customers = new GameObject[maxNumCustomer];
-    public Vector2[] CustomerVectors = new Vector2[maxNumCustomer];
+    public Vector2[] LineSpot = new Vector2[maxNumCustomer];
 
     public GameObject customer_prefab;
 
 
 
 
-    //spawn numberof customers outside the door, one at a time
+    /*=======================
+     * Start spawning customer 1 at a time
+     * =========================*/
     public void startDay()
     {
-
         StartCoroutine(SpwanCustomer());
-        
-            
     }
-    IEnumerator SpwanCustomer()
+    IEnumerator SpwanCustomer()//part of above
     {
         for (int index = 0; index < maxNumCustomer; index++)
         {
             yield return new WaitForSeconds(3);
             customers[index] = Instantiate(customer_prefab, new Vector3(-3, -10, 0), Quaternion.identity);
-            customers[index].GetComponent<InlineCustomerBehavior>().init( CustomerVectors[index]);
+            customers[index].GetComponent<InlineCustomerBehavior>().moveUp( LineSpot[index]);
         }
     }
 
+
+
+    /*================================
+     * have the first customer to leave
+     * and moves up the whole line by 1
+     * then spawn 1 customer at the back of the line
+     * ==============================*/
     public void MoveLine()
     {
         customers[totalCustomer % maxNumCustomer].GetComponent<InlineCustomerBehavior>().leave();
-        StartCoroutine(delayedMove());
         customers[totalCustomer % maxNumCustomer] = Instantiate(customer_prefab, new Vector3(-2, -10, 0), Quaternion.identity);
-        customers[totalCustomer % maxNumCustomer].GetComponent<InlineCustomerBehavior>().init(CustomerVectors[maxNumCustomer - 1]);
+        customers[totalCustomer % maxNumCustomer].GetComponent<InlineCustomerBehavior>().moveUp(LineSpot[maxNumCustomer - 1]);
         totalCustomer++;
+        StartCoroutine(delayedMove());
     }
-    IEnumerator delayedMove()
+    IEnumerator delayedMove()//part of above
     {
-        for (int i = 1; i < maxNumCustomer - 1; i++)
+        for (int i = 0; i < maxNumCustomer - 1; i++)
         {
-            yield return new WaitForSeconds(0.5f);
-            customers[(totalCustomer + i) % maxNumCustomer].GetComponent<InlineCustomerBehavior>().TargetLoc = CustomerVectors[i - 0];
+            customers[(totalCustomer + i) % maxNumCustomer].GetComponent<InlineCustomerBehavior>().moveUp(LineSpot[i]);
+            yield return new WaitForSeconds(0.4f);
         }
+    }
+
+
+
+    /*=======================
+     * Returns total number of customers served
+    * =========================*/
+    public int getTotalCustomers()
+    {
+        return totalCustomer;
     }
 
     // Start is called before the first frame update
@@ -66,11 +74,7 @@ public class LineSystem : MonoBehaviour
     }
 
 
-    //returns the total number of customers served;
-    public int getTotalCustomers()
-    {
-        return totalCustomer;
-    }
+
 
     // Update is called once per frame
     void Update()
