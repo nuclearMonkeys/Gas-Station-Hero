c07753a347bac8bc4d3a618ff9534f3db6f2f13b
Ulleseit <ulleseit99@gmail.com>
2020-04-21 20:13:04 -0700

    Results variables
    Added a script with variables that will be used with results screen, as well as a test scene to mess around with a canvas for the results screen.

---
+++ Gas-Station-Hero/Assets/_Scripts/Results.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Results : MonoBehaviour
+{
+	public int customers = 0;
+	public int savings = 0;
+	public int goal;
+	public int rent;
+	public int utilities;
+	public int day;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Show screen
+        savings = calculateTotal();
+		customers = 0;
+		//Reset and progress day
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	int calculateTotal()
+	{
+		savings += customers * 800;
+		savings -= rent;
+		savings -= utilities;
+		return savings;
+	}
+}
