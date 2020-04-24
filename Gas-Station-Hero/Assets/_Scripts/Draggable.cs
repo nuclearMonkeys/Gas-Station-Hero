aa0c35f1af8a16f0279849444f4a25c9e9925892
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-04-24 14:35:45 -0700

    Items returning to original location implemented

--- Gas-Station-Hero/Assets/_Scripts/Draggable.cs
+++ Gas-Station-Hero/Assets/_Scripts/Draggable.cs
@@ -5,27 +5,39 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    
     public Vector2 ReturningPosition;
     public Vector2 CursorOffset;
-    public float MaxSpeed = 2;
+    public bool isOverCounter;
+
+
+    private Transform parent;
     private float updateSpeedSec = 0.7f;
-    private bool isOverCounter;
     private bool CanBeGiven;
     private bool falling = false;
-    
+    private float speed;
+
     private void Update() 
     {
         if(falling) 
         {
+            Debug.Log("working");
             if (0.1f < ((Vector2)transform.position - ReturningPosition).magnitude)
             {
-                transform.position = Vector2.MoveTowards(transform.position, ReturningPosition, MaxSpeed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(transform.position, ReturningPosition, speed * Time.deltaTime);
+            }
+            else
+            {
+                falling = false;
             }
         }
     }
 
     public void OnBeginDrag(PointerEventData eventData) 
     {
+        parent = transform.parent;
+        //Debug.Log(transform.position);
+        transform.SetParent(parent.parent); 
         ReturningPosition = transform.position;
         CursorOffset = eventData.position - (Vector2)transform.position;
         GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -38,11 +50,15 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     public void OnEndDrag(PointerEventData eventData) 
     {
+        transform.SetParent(parent);
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+        speed = ((Vector2)transform.position - ReturningPosition).magnitude*10;
+        falling = true;
+        /*
         if (isOverCounter)
         {
-            //
-        }
+            
+        }*/
     }
 
     private IEnumerator DropObject() 
