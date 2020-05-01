aa7ade88005271597e7ced461ce82636fb0ecbc6
Ulleseit <ulleseit99@gmail.com>
2020-05-01 13:56:28 -0700

    Scanner item
    Made a scanner item that can read objects that it moves over, and detect if they are named the same name as a product.

---
+++ Gas-Station-Hero/Assets/_Scripts/Customer Interaction/Scannable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scannable : MonoBehaviour
+{
+	public GameObject scanner;
+	bool scanned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!scanned && gameObject.transform.position.x > scanner.transform.position.x-20 && gameObject.transform.position.x < scanner.transform.position.x+20 && gameObject.transform.position.y > scanner.transform.position.y-20 && gameObject.transform.position.y < scanner.transform.position.y+20)
+		{
+			scanner.GetComponent<Scanner>().scanItem(gameObject.name);
+			scanned = true;
+		}
+    }
+}
---
+++ Gas-Station-Hero/Assets/_Scripts/Customer Interaction/Scanner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scanner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+	public void scanItem(string name)
+	{
+		if(name == "Candy")
+		{
+			Debug.Log("Candy");
+		}
+		else if(name == "Chips")
+		{
+			Debug.Log("Chips");
+		}
+		else if(name == "Alcohol")
+		{
+			Debug.Log("Alcohol");
+		}
+		else if(name == "Cigarettes")
+		{
+			Debug.Log("Cigarettes");
+		}
+	}
+}
