c82233a15f85107c947b9a7c817eee57c0c07349
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-05-31 15:15:34 -0700

    tweaked the Register

---
+++ Gas-Station-Hero/Assets/openCashCompartment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class openCashCompartment : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.GetChild(3).gameObject.SetActive(!transform.GetChild(3).gameObject.activeSelf);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
