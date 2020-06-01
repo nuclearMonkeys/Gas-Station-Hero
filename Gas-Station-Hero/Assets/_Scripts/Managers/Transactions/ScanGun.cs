b0be25c046b1c67d813fb32b68e7a69f7e9d71ff
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-06-01 16:45:24 -0700

    got rid of the debugging prints

--- Gas-Station-Hero/Assets/_Scripts/Managers/Transactions/ScanGun.cs
+++ Gas-Station-Hero/Assets/_Scripts/Managers/Transactions/ScanGun.cs
@@ -7,19 +7,16 @@ public class ScanGun : MonoBehaviour
     public  CashRegister REGISTER;
     public void scanning(float price, GameObject item)
     {
-        print(item.tag);
 		if(item.tag == "Respawn" && item.transform.GetChild(1).GetComponent<DiscountBarcode>().canBeScanned && !item.transform.GetChild(1).GetComponent<Barcode>().alreadyScanned)
 		{
 			REGISTER.scanned(price);
 			item.transform.GetChild(1).GetComponent<DiscountBarcode>().alreadyScanned = true;
 			item.transform.GetChild(1).GetComponent<DiscountBarcode>().customerNotEnoughMoney = false;
-            print("DISCOUNT SCANNED");
+
 		}
 		else if(item.tag != "Respawn" && !item.transform.GetChild(1).GetComponent<Barcode>().alreadyScanned)
 		{
 			REGISTER.scanned(price);
-            print("ITEM SCANNED");
-            print(item.name);
 			item.transform.GetChild(1).GetComponent<Barcode>().alreadyScanned = true;
 			item.GetComponent<Draggable>().canBeGiven = true;
 			REGISTER.scannedItems.Add(item.GetComponent<Draggable>());
